b5ab24df3acc327655c64378c00f3b960b9a35be
Thanh Hoàng <fightforml@gmail.com>
2023-03-01 10:40:35 +0700

    Update local kafka node

---
+++ kafka/producer/index.js
@@ -0,0 +1,24 @@
+const kafka = require("kafka-node");
+
+const client = new kafka.KafkaClient({
+  kafkaHost: "http://157.230.36.232:19092",
+  connectTimeout: 10000,
+  requestTimeout: 10000,
+});
+const producer = new kafka.Producer(client);
+
+const km = new kafka.KeyedMessage("key", "message");
+const payloads = [
+  { topic: "topic1", messages: "hi", partition: 0 },
+  { topic: "topic2", messages: ["hello", "world", km] },
+];
+producer.on("ready", function () {
+  producer.send(payloads, function (err, data) {
+    console.log("🚀 ~ file: producer.js:40 ~ data:", data);
+    console.log("🚀 ~ file: producer.js:40 ~ err:", err);
+  });
+});
+
+producer.on("error", function (err) {
+  console.log("🚀 ~ file: producer.js:48 ~ err:", err);
+});
